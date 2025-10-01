b77aec606df882df290e9fba4472291d3b6e66ec
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-10-01 17:44:48 +0200

    impostata struttura con bootstrap e select per genere (ancora senza logica)

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
 import movies from "./components/movies";
 
 function App() {
-  const [count, setCount] = useState(0);
+  //valore scelto nella select ("" = tutti)
+  const [selectedGenre, setSelectedGenre] = useState("");
+
+  //lista che viene mostrata
+  const [filteredMovies, setFilteredMovies] = useState(movies);
 
   return (
     <>
       <main>
         <h1>React Movie Filter</h1>
 
+        {/* labels */}
+        <div>
+          {/* 1. Seleziona un genere */}
+          <div class="mb-3">
+            <label for="disabledSelect" class="form-label">
+              Seleziona il genere che vuoi
+            </label>
+            <select id="disabledSelect" class="form-select">
+              <option>Seleziona un genere</option>
+            </select>
+          </div>
+        </div>
+
         {/* film */}
-        <ul>
-          {movies.map((movie, index) => (
-            <li key={index}>
-              {movie.title} &minus; {movie.genre}
-            </li>
-          ))}
-        </ul>
+        <div>
+          <ul>
+            {movies.map((movie, index) => (
+              <li key={index}>
+                {movie.title} &minus; {movie.genre}
+              </li>
+            ))}
+          </ul>
+        </div>
       </main>
     </>
   );
